d757b49605f1ebd7301671c490e626c5d2c990bb
Fallen <axlsntz.dev@gmail.com>
2023-10-16 17:57:10 -0600

    add config index (12-13)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import indexRoutes from './routes/index.routes';
 
 const app = express();
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 4001;
 // Config
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 // Routes
 app.use('/api', indexRoutes);
