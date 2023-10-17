21c7a73b0127da72f338cce56e6191969f50dbf6
Fallen <axlsntz.dev@gmail.com>
2023-10-16 18:59:29 -0600

    add auth controller

--- server/src/modules/auth/services/auth.services.ts
+++ server/src/modules/auth/services/auth.services.ts
@@ -1,20 +1,49 @@
 import bcrypt from 'bcrypt';
-import { database } from '../../../database';
+import jwt from 'jsonwebtoken';
 import { UserCredentials } from '../interfaces';
+import { createUser, getUserByEmail } from '../../user/services';
+import { IUser, UserDTO } from '../../user/interfaces';
 
 export const validateUser = async (credentials: UserCredentials) => {
- try {
-    const { email, password } = credentials;
-    const user = await database.oneOrNone('SELECT * FROM users WHERE email = $1', [email]);
-    if (!user) {
-        throw new Error('User not found');
+    try {
+        const { email, password } = credentials;
+        const user = await getUserByEmail(email);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            throw new Error('Invalid password');
+        }
+        const token = generateToken(user);
+        return { msg: 'Login successfull', token };
+    } catch (error) {
+        throw new Error(error);
     }
-    const valid = await bcrypt.compare(password, user.password);
-    if (!valid) {
-        throw new Error('Invalid password');
-    }
-    return user;
- } catch (error) {
-    throw new Error(error);
- }
 };
+
+export const registerUser = async (user: UserDTO) => {
+    try {
+        const userFound = await getUserByEmail(user.email);
+        if (userFound) {
+            throw new Error('User already exists');
+        }
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+        const newUser = await createUser(user);
+        const token = generateToken(newUser);
+        return {msg: 'User created successfully', token};
+
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+export const generateToken = (user: IUser) => {
+    const payload = {
+        user_id: user.user_id,
+
+    };
+    return jwt.sign(payload, process.env.SECRET_KEY || 'secret', { expiresIn: '1h' });
+};
+
