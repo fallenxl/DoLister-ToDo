ddba0c5df40979d5ea22a430125c60c57c4b97cb
Fallen <axlsntz.dev@gmail.com>
2023-10-16 23:50:40 -0600

    add login and register form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Login from "./pages/auth/Login"
+import Register from "./pages/auth/Register"
 function App() {
 
   return (
-    <>
-      <main></main>
-      
-    </>
+   <Router>
+    <Routes>
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+      <Route path="/" element={<h1>Home</h1>} />
+    </Routes>
+   </Router>
   )
 }
 
