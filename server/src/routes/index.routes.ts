ddf4c180c596e2c9f5b980d819559a6df48f5a9d
Fallen <axlsntz.dev@gmail.com>
2023-10-16 18:12:31 -0600

    add auth, task routes

--- server/src/routes/index.routes.ts
+++ server/src/routes/index.routes.ts
@@ -2,4 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
+router.use('/auth', require('./auth/auth.routes'));
+router.use('/tasks', require('./task/tasks.routes'));
+
 export default router;
