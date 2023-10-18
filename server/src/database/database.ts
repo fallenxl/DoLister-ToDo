b35871f184d128cc0fe8f9f82a7e2d078d41612b
Fallen <axlsntz.dev@gmail.com>
2023-10-18 14:54:38 -0600

    Add changes

--- server/src/database/database.ts
+++ server/src/database/database.ts
@@ -5,7 +5,13 @@ import 'dotenv/config';
 import pgp from 'pg-promise';
 
 // Configura la conexión a la base de datos utilizando las variables de entorno
-const db = pgp()(process.env.DB_URL as string);
+const db = pgp()({
+    user: process.env.DB_USER, // Nombre de usuario de la base de datos
+    password: process.env.DB_PASSWORD, // Contraseña del usuario de la base de datos
+    host: process.env.DB_HOST, // Host de la base de datos
+    port: Number(process.env.DB_PORT), // Puerto de la base de datos (convertido a número)
+    database: process.env.DB_NAME, // Nombre de la base de datos
+});
 
 // Exporta la conexión a la base de datos para su uso en otros archivos
 export default db;
