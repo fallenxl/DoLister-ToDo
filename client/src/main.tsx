a3448b1210a4bf596f0e2312d9d5d5b9e81f8b9f
FallEn <axlsntz.dev@gmail.com>
2023-12-28 08:25:30 -0600

    modify src/main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ThemeProvider } from '@material-tailwind/react'
-import { AxiosInterceptor } from './interceptor/axios.interceptor.ts'
-AxiosInterceptor()
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
